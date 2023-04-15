5dcec4ed2982ca39ce151f12898dbd3cf161688e
Richard Posada <riposada@microsoft.com>
2023-04-15 10:03:48 -0400

    update

---
+++ 02 - Web UI Template/CognitiveSearch.UI/Controllers/CaseManagementController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CognitiveSearch.UI.Controllers
+{
+    public class CaseManagementController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
